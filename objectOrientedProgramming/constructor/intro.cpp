054f898a4af6a644f7322d68d6f52f9c48151a86
viveknapit <76894610+viveknapit@users.noreply.github.com>
2024-03-16 22:58:28 +0530

    different types of constructor

--- objectOrientedProgramming/constructor/intro.cpp
+++ objectOrientedProgramming/constructor/intro.cpp
@@ -9,6 +9,14 @@ public:
     string productName;
 
 public:
+    // default constructor
+    SalesData()
+    {
+        price = 10000;
+        productId = 101;
+        productName = "prodoct_101";
+    }
+
     // Parameterized  constructor
     SalesData(int price, int productId, string productName)
     {
@@ -16,10 +24,19 @@ public:
         this->productId = productId;
         this->productName = productName;
     }
+
+    // copy constructor
+    // SalesData(SalesData &obj)
+    // {
+    //     price = obj.price;
+    //     productId = obj.productId;
+    //     productName = obj.productName;
+    // }
 };
 
 int main()
 {
     SalesData data1 = SalesData(1111, 100, "apple");
+    SalesData data2 = data1;
     cout << data1.price << endl;
 }
